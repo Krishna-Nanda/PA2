e86eadb9bfc8e4194e85baf4b8f77cbb3c50e22a
Krishna Nanda <krishnananda@ucsb.edu>
2019-11-16 11:07:23 -0800

    Adding original starter code

---
+++ FeatureGraph.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include "FeatureGraph.h"
+#include "GraphHelper.h"
+
+
+using namespace std;
+
+
+FeatureGraph::FeatureGraph(int N, int d, vector<Node> nodes, vector<Edge> edges) {
+    //TODO
+};
+
+void FeatureGraph::insert(Node node){
+    //TODO
+};
+    
+void FeatureGraph::insert(Edge edge){
+    //TODO
+};
+
+
